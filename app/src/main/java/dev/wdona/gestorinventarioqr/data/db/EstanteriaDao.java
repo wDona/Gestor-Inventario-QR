eb5ebbbc91e234bf4919bf38275e78eeb26c964b
miaudona <donanferyt@gmail.com>
2026-02-17 09:44:57 +0100

    chore: Hacer las consultas del DAO

--- app/src/main/java/dev/wdona/gestorinventarioqr/data/db/EstanteriaDao.java
+++ app/src/main/java/dev/wdona/gestorinventarioqr/data/db/EstanteriaDao.java
@@ -1,7 +1,10 @@
 package dev.wdona.gestorinventarioqr.data.db;
 
+import androidx.room.Query;
+
 import dev.wdona.gestorinventarioqr.domain.model.Estanteria;
 
 public interface EstanteriaDao {
+    @Query("SELECT * FROM Estanteria WHERE id = :id")
     Estanteria getEstanteriaById(int id);
 }
--- app/src/main/java/dev/wdona/gestorinventarioqr/data/db/ProductoDao.java
+++ app/src/main/java/dev/wdona/gestorinventarioqr/data/db/ProductoDao.java
@@ -1,11 +1,19 @@
 package dev.wdona.gestorinventarioqr.data.db;
 
+import androidx.room.Query;
+
 import dev.wdona.gestorinventarioqr.domain.model.Estanteria;
 import dev.wdona.gestorinventarioqr.domain.model.Producto;
 
 public interface ProductoDao {
-    void addUndsProduct(Producto producto);
-    void removeUndsProduct(Producto producto);
-    void assignProductToEstanteria(Producto producto, Estanteria estanteria);
+    @Query("UPDATE Producto SET cantidad = cantidad + :cantidad WHERE id = :productoId")
+    void addUndsProduct(Long productoId, int cantidad);
+
+    @Query("UPDATE Producto SET cantidad = cantidad - :cantidad WHERE id = :productoId")
+    void removeUndsProduct(Long productoId, int cantidad);
+    @Query("UPDATE Producto SET FK_estanteriaId = :estanteriaId WHERE id = :productoId")
+    void assignProductToEstanteria(Long productoId, Long estanteriaId);
+
+    // TODO
     void addPendienteProduct(Producto producto);
 }
