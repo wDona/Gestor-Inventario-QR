4c59aeb06f7271abac6dfe6524a5df8034472286
miaudona <donanferyt@gmail.com>
2026-02-17 09:44:17 +0100

    refactor: cambiar nombre de la tabla de ProductosEntity

--- app/src/main/java/dev/wdona/gestorinventarioqr/data/entity/ProductoEntity.java
+++ app/src/main/java/dev/wdona/gestorinventarioqr/data/entity/ProductoEntity.java
@@ -5,7 +5,7 @@ import androidx.room.ForeignKey;
 import androidx.room.Index;
 
 @Entity(
-        tableName = "productos",
+        tableName = "Producto",
         foreignKeys = @ForeignKey(
                 entity = EstanteriaEntity.class,
                 parentColumns = "id",
